f450f077187903313af4fa3f0fbe307022e68d48
Valentas Klimas <valentasklimas@gmail.com>
2021-02-10 16:00:50 +0000

    DB context added

---
+++ ResizeImageConsoleAPP/Models/Image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ResizeImageConsoleAPP.Models
+{
+    public partial class Image
+    {
+        public int Id { get; set; }
+        public string ImageId { get; set; }
+        public string UserName { get; set; }
+        public bool Resized { get; set; }
+        public int ImageHeight { get; set; }
+        public int ImageWidth { get; set; }
+    }
+}
