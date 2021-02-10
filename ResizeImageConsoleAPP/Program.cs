7f4ea48bb53d71fb7069064bc0e0802fddce6872
Valentas Klimas <valentasklimas@gmail.com>
2021-02-10 21:37:27 +0000

    change data in db

--- ResizeImageConsoleAPP/Program.cs
+++ ResizeImageConsoleAPP/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ResizeImageConsoleAPP.Models;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ResizeImageConsoleAPP
 {
@@ -7,7 +10,18 @@ namespace ResizeImageConsoleAPP
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hello World!");
+            ResizeImage resize = new ResizeImage();
+            while (true)
+            {              
+                if (resize.getOneImageDetails().Id != 0)
+                {
+                    Image imagedetails = resize.getOneImageDetails();
+                    Console.WriteLine($"Image ID: {imagedetails.Id}, Image size:" + $" {imagedetails.ImageHeight} x {imagedetails.ImageWidth}");
+                    resize.changeImageStatus(imagedetails);
+                }
+              
+            }         
+          
         }
     }
 }
---
+++ ResizeImageConsoleAPP/ResizeImage.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResizeImageConsoleAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ResizeImageConsoleAPP
+{
+    class ResizeImage
+    {
+        private readonly SaveImageToFolderContext _context;
+        public ResizeImage()
+        {
+            _context = new SaveImageToFolderContext();
+        }
+
+        public Image getOneImageDetails()
+        {
+            if (_context.Images.FirstOrDefault((x) => x.Resized == false) == null)
+            {
+                return new Image() { Id = 0 };
+            }
+
+            Image imageDetails = _context.Images.FirstOrDefault((x) => x.Resized == false);
+            return imageDetails;
+        }
+
+        public void resizeImage()
+        {
+        }
+
+        public void changeImageStatus(Image image)
+        {
+            image.Resized = true;
+
+            _context.Entry(image).State = EntityState.Modified;
+            _context.SaveChanges();
+               
+        }
+    }
+}
